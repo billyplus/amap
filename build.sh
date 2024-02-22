3a6716e361d127792cb970c263b74acb73766e67
billy <me@ybilly.com>
2024-02-22 16:35:32 +0800

    修复格式

--- build.sh
+++ build.sh
@@ -3,22 +3,21 @@
 APK_URL='https://mapdownload.autonavi.com/apps/auto/manual/V750/Auto_7.5.0.600056_beta.apk'
 APK_RELEASE_NAME='Auto_7.5.0.600056_beta_clone.apk'
 PACKAGE_NAME='com.autonavi.amapautoclone' # 包名
-APP_NAME='高德比亚迪' # App显示名称
-FIXED_CHANNEL='on' # 锁定13通道，on表示锁定，off表示不锁定
+APP_NAME='高德比亚迪'                          # App显示名称
+FIXED_CHANNEL='on'                        # 锁定13通道，on表示锁定，off表示不锁定
 
 mkdir ./out
 curl -sL -o amap.apk ${APK_URL}
 
 apktool d amap.apk
-sed -i 's/    const\/4 p0, 0x0/    const\/4 p0, 0x1/' `grep -ril "ApkSignUtil.java" ./amap/`
+sed -i 's/    const\/4 p0, 0x0/    const\/4 p0, 0x1/' $(grep -ril "ApkSignUtil.java" ./amap/)
 
-if [ $FIXED_CHANNEL='on' ]
-then
+if [ $FIXED_CHANNEL='on' ]; then
     sed -i '/^    iput p1, p0/i\    const\/16 p1, 0xE\n' ./amap/smali/com/autonavi/amapauto/jni/config/AudioConfigData.smali
     sed -i '/^    return v1/i\    const\/16 v1, 0xE\n' ./amap/smali/com/autonavi/amapauto/jni/config/AudioConfigData.smali
     sed -i '/^    const\/4 v0, 0x0/i\    const\/16 v0, 0xE\n' ./amap/smali/com/autonavi/amapauto/jni/config/AudioConfigData.smali
     sed -i '/^    const\/4 v0, 0x0/i\    iput v0, p0, Lcom\/autonavi\/amapauto\/jni\/config\/AudioConfigData;->audioChannel:I\n' ./amap/smali/com/autonavi/amapauto/jni/config/AudioConfigData.smali
-end
+fi
 
 sed -i "s/package=\"com.autonavi.amapauto/package=\"${PACKAGE_NAME}/" ./amap/AndroidManifest.xml
 sed -i "s/com.autonavi.amapauto.permission/${PACKAGE_NAME}.permission/" ./amap/AndroidManifest.xml
